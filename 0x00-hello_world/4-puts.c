d62d44879cf9b5c9e56c458b96e4ee7363ed5476
Ayoubmansour <Ayoub.mansour8@gmail.com>
2023-06-16 08:55:09 -0700

    #include <stdio.h>

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,23 +1,13 @@
-export CFILE="main.c"
-./compile_script.sh
-#!/bin/bash
+#include <stdio.h>
 
-# Retrieve the C file name from the CFILE environment variable
-c_file="$CFILE"
-
-# Compile the C file and create the executable
-gcc "$c_file" -o cisfun
-
-# Check if the compilation was successful
-if [ $? -eq 0 ]; then
-  echo "Programming is like building a multilingual puzzle"
-  echo ""
-  echo "Size of a char: $(sizeof char) byte(s)"
-  echo "Size of an int: $(sizeof int) byte(s)"
-  echo "Size of a long int: $(sizeof long int) byte(s)"
-  echo "Size of a long long int: $(sizeof long long int) byte(s)"
-  echo "Size of a float: $(sizeof float) byte(s)"
-else
-  echo "Compilation failed."
-fi
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    puts("\"Programming is like building a multilingual puzzle");
+    return (0);
+}
 
