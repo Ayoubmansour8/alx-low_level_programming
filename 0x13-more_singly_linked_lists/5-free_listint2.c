545fd01cedd659c927cb47574fb820f568ea1756
Ayoubmansour8 <ayoub.mansour8@gmail.com>
2023-08-02 11:50:55 -0700

    all

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -6,19 +6,19 @@
  */
 void free_listint2(listint_t **head)
 {
-	if (head == NULL || *head == NULL)
-		return;
+ listint_t *temp;
 
-	listint_t *current = *head;
-	listint_t *next;
+ if (head == NULL)
+ return;
 
-	while (current)
-	{
-		next = current->next;
-		free(current);
-		current = next;
-	}
+ while (*head)
+ {
+ temp = (*head)->next;
+ free(*head);
+ *head = temp;
+ }
 
-	*head = NULL;
+ *head = NULL;
 }
+
 
