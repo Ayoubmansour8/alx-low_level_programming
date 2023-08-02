acbb349c7263feb21f1dbd3232caa0292a21fac2
Ayoubmansour8 <ayoub.mansour8@gmail.com>
2023-08-02 12:15:11 -0700

    all

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -12,14 +12,12 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	unsigned int i = 0;
 	listint_t *temp = head;
 
-	/* Traverse the linked list until the end or the desired index is reached */
 	while (temp && i < index)
 	{
 		temp = temp->next;
 		i++;
 	}
 
-	/* Return the node at the desired index, or NULL if the index is beyond the list */
-	return (temp ? temp : NULL);
+	return (temp);
 }
 
