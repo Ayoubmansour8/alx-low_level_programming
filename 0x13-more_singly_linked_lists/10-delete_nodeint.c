8c2c19f981b272b5945155703471816f27f8ec04
Ayoubmansour8 <ayoub.mansour8@gmail.com>
2023-08-02 12:24:25 -0700

    all

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -9,22 +9,20 @@
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
+	listint_t *temp = *head;
+	listint_t *current = NULL;
 	unsigned int i = 0;
-	listint_t *temp, *current;
 
-	if (*head == NULL) // If the list is empty
+	if (*head == NULL)
 		return (-1);
 
-	if (index == 0) // If the node to be deleted is the first node
+	if (index == 0)
 	{
-		temp = *head;
 		*head = (*head)->next;
 		free(temp);
 		return (1);
 	}
 
-	// Traverse to the node just before the one to be deleted
-	temp = *head;
 	while (i < index - 1)
 	{
 		if (!temp || !(temp->next))
@@ -33,7 +31,6 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		i++;
 	}
 
-	// Perform the deletion
 	current = temp->next;
 	temp->next = current->next;
 	free(current);
