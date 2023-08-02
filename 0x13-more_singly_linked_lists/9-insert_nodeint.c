ba53ad71d7ea918559bfcc820437fbbb4f4137b4
Ayoubmansour8 <ayoub.mansour8@gmail.com>
2023-08-02 11:19:40 -0700

    9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -15,16 +15,13 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *new;
 	listint_t *temp = *head;
 
-	/* Allocate memory for the new node */
 	new = malloc(sizeof(listint_t));
 	if (!new || !head)
 		return (NULL);
 
-	/* Set the data and next pointer of the new node */
 	new->n = n;
 	new->next = NULL;
 
-	/* Insert the new node at the beginning of the list */
 	if (idx == 0)
 	{
 		new->next = *head;
@@ -32,13 +29,10 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (new);
 	}
 
-	/* Traverse the list to find the node before the insertion point */
 	for (i = 0; temp && i < idx; i++)
 	{
-		/* If the node before the insertion point is found */
 		if (i == idx - 1)
 		{
-			/* Insert the new node between temp and temp->next */
 			new->next = temp->next;
 			temp->next = new;
 			return (new);
@@ -47,7 +41,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			temp = temp->next;
 	}
 
-	/* If the insertion point is out of bounds, return NULL */
 	return (NULL);
 }
 
