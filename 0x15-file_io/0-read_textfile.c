63198001a70b8db3c121b744d544942165600070
Ayoubmansour8 <ayoub.mansour8@gmail.com>
2023-08-07 16:22:53 -0700

    all

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,59 +1,31 @@
 #include "main.h"
 #include <stdlib.h>
-#include <stdio.h>
-#include <fcntl.h>
 #include <unistd.h>
+#include <fcntl.h>
 
 /**
- * read_textfile - Read text file and print to STDOUT.
- * @filename: Text file being read.
- * @letters: Number of letters to be read.
- * Return: The actual number of bytes read and printed.
+ * read_textfile- Read text file print to STDOUT.
+ * @filename: text file being read
+ * @letters: number of letters to be read
+ * Return: w- actual number of bytes read and printed
  *         0 when function fails or filename is NULL.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	if (filename == NULL)
-	{
-		fprintf(stderr, "Error: Null filename\n");
-		return (0);
-	}
+	char *buf;
+	ssize_t fd;
+	ssize_t w;
+	ssize_t t;
 
-	int fd = open(filename, O_RDONLY);
+	fd = open(filename, O_RDONLY);
 	if (fd == -1)
-	{
-		perror("Error opening file");
 		return (0);
-	}
+	buf = malloc(sizeof(char) * letters);
+	t = read(fd, buf, letters);
+	w = write(STDOUT_FILENO, buf, t);
 
-	char *buffer = (char *)malloc(sizeof(char) * letters);
-	if (buffer == NULL)
-	{
-		close(fd);
-		perror("Memory allocation error");
-		return (0);
-	}
-
-	ssize_t bytes_read = read(fd, buffer, letters);
-	if (bytes_read == -1)
-	{
-		perror("Error reading file");
-		free(buffer);
-		close(fd);
-		return (0);
-	}
-
-	ssize_t bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-	if (bytes_written == -1)
-	{
-		perror("Error writing to STDOUT");
-		free(buffer);
-		close(fd);
-		return (0);
-	}
-
-	free(buffer);
+	free(buf);
 	close(fd);
-	return bytes_written;
+	return (w);
 }
 
--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,53 +1,105 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+#define BUFFER_SIZE 4096
+
+void copy_file(const char *src_file, const char *dest_file);
+int open_file(const char *file, int flags, mode_t mode);
+void close_file(int fd);
+void handle_error(const char *msg);
 
 /**
- * read_textfile - Read text file and print to STDOUT.
- * @filename: Text file being read.
- * @letters: Number of letters to be read.
+ * main - Copies the contents of a file to another file.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
  *
- * Return: Number of bytes read and printed.
- *         0 when the function fails or filename is NULL.
+ * Return: 0 on success, otherwise an appropriate exit code.
+ *
+ * Description: If the argument count is incorrect - exit code 97.
+ * If file_from does not exist or cannot be read - exit code 98.
+ * If file_to cannot be created or written to - exit code 99.
+ * If file_to or file_from cannot be closed - exit code 100.
  */
-ssize_t read_textfile(const char *filename, size_t letters)
+int main(int argc, char *argv[])
 {
-	int fd;
-	char *buffer;
-	ssize_t bytes_read, bytes_written;
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
 
-	if (filename == NULL)
-		return (0);
+	copy_file(argv[1], argv[2]);
 
-	fd = open(filename, O_RDONLY);
+	return (0);
+}
+
+/**
+ * copy_file - Copies the contents of a file to another file.
+ * @src_file: The name of the source file.
+ * @dest_file: The name of the destination file.
+ */
+void copy_file(const char *src_file, const char *dest_file)
+{
+	int from, to;
+	ssize_t r, w;
+	char buffer[BUFFER_SIZE];
+
+	from = open_file(src_file, O_RDONLY, 0);
+	to = open_file(dest_file, O_CREAT | O_WRONLY | O_TRUNC, 0664);
+
+	while ((r = read(from, buffer, BUFFER_SIZE)) > 0)
+	{
+		w = write(to, buffer, r);
+		if (w != r)
+			handle_error("Error: Can't write to destination file");
+	}
+
+	if (r == -1)
+		handle_error("Error: Can't read from source file");
+
+	close_file(from);
+	close_file(to);
+}
+
+/**
+ * open_file - Opens a file and handles errors if any.
+ * @file: The name of the file to open.
+ * @flags: The flags to use when opening the file.
+ * @mode: The file mode to set if the file is created.
+ *
+ * Return: The file descriptor on success, otherwise exits with an error code.
+ */
+int open_file(const char *file, int flags, mode_t mode)
+{
+	int fd = open(file, flags, mode);
 	if (fd == -1)
-		return (0);
+		handle_error("Error: Can't open file");
 
-	buffer = malloc(sizeof(char) * letters);
-	if (buffer == NULL)
-	{
-		close(fd);
-		return (0);
-	}
+	return fd;
+}
 
-	bytes_read = read(fd, buffer, letters);
-	if (bytes_read == -1)
-	{
-		close(fd);
-		free(buffer);
-		return (0);
-	}
+/**
+ * close_file - Closes a file descriptor and handles errors if any.
+ * @fd: The file descriptor to be closed.
+ */
+void close_file(int fd)
+{
+	int c = close(fd);
 
-	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-	if (bytes_written != bytes_read)
-	{
-		close(fd);
-		free(buffer);
-		return (0);
-	}
+	if (c == -1)
+		handle_error("Error: Can't close file descriptor");
+}
 
-	free(buffer);
-	close(fd);
-	return (bytes_written);
+/**
+ * handle_error - Prints an error message and exits the program with an error code.
+ * @msg: The error message to be printed.
+ */
+void handle_error(const char *msg)
+{
+	dprintf(STDERR_FILENO, "%s\n", msg);
+	exit(99);
 }
 
