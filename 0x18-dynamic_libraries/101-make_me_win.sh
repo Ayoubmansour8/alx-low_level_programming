2445444ddd8801c9eea6f1a59b01a909376b49fa
Ayoubmansour8 <ayoub.mansour8@gmail.com>
2023-09-19 08:46:22 -0700

    all

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-Wget -p /tmp https://github.com/Ayoubmansour8/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libgiga.so
-export LD PRELOAD=/tmp/libgiga.so
+Wget -P /tmp https://github.com/Ayoubmansour8/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libgiga.so
+export LD_PRELOAD=/tmp/libgiga.so
